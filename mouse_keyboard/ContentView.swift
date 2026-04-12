82caf7394d78e9ad0f4b829610d0e21e813c3b7f
neptuno <xumincheng123@gmail.com>
2026-04-12 16:47:42 +0800

    Initial Commit

---
+++ mouse_keyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mouse_keyboard
+//
+//  Created by 程旭敏 on 4/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mouse_keyboard/mouse_keyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouse_keyboardApp.swift
+//  mouse_keyboard
+//
+//  Created by 程旭敏 on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct mouse_keyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
